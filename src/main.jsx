0ab5a8f4795c31ed6ed8a16310af52a27d69eed1
douglanyabasa <douglasnyabasa400@gmail.com>
2025-09-07 10:50:47 +0200

    updates on redux1

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
+import productReducer  from "./redux/productSlice.jsx"
 
 const store = configureStore({
-  reducer:{},
+  reducer:{
+    products : productReducer
+  },
 })
 createRoot(document.getElementById('root')).render(
   <StrictMode>
