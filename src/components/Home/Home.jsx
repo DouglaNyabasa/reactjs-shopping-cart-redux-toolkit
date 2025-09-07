fa099b1591689df96cd9ec5ea10044a26b93fc56
douglanyabasa <douglasnyabasa400@gmail.com>
2025-09-07 13:59:47 +0200

    added products

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { useGetAllProductsQuery } from '../../redux/productApi';
+import { productsApi, useGetAllProductsQuery } from '../../redux/productApi';
+
 
 const Home = () => {
   const {data, error,isLoading} = useGetAllProductsQuery()
   return (
-    <div>
-      <h2>home</h2>
-    </div>
+    <div  className='home-container'>
+        {isLoading ? <p>Loading ....</p> : error ? <p>An Error Occured...</p> : (
+          <>
+            <h2>New Arrivals </h2>
+             <div className='products'>
+                  {data?.map(product => <div key={product.id} className='product'> 
+                     <h3>{productsApi.name}</h3>
+                     <img src={product.image} alt={product.name}/>
+                     <div className="details">
+                      <span>{product.desc}</span>
+                      <span className='price'>{product.price}</span>
+                     </div>
+                     <button>Add To Cart</button>
+                  </div>)}
+             </div>
+          </>
+        )}
+        </div>
   );
 }
 
