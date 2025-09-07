0d3f81c046e30f104a4363c5f21c0ed41a54bf6a
douglanyabasa <douglasnyabasa400@gmail.com>
2025-09-07 17:48:37 +0200

    navigate to cart

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
-import {  useGetAllProductsQuery } from '../../redux/productApi';
+import { useGetAllProductsQuery } from '../../redux/productApi';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../../redux/cartsSlice';
-
+import { useNavigate } from "react-router-dom"; 
 
 const Home = () => {
-  const {data, error,isLoading} = useGetAllProductsQuery()
+  const { data, error, isLoading } = useGetAllProductsQuery();
   const dispatch = useDispatch();
-  const handleAddToCart = (product) =>{
-       dispatch(addToCart(product))
-  }
+  const navigate = useNavigate();
+
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+    navigate("/cart"); 
+  };
+
   return (
-    <div  className='home-container'>
-        {isLoading ? <p>Loading ....</p> : error ? <p>An Error Occured...</p> : (
-          <>
-            <h2>New Arrivals </h2>
-             <div className='products'>
-                  {data?.map(product => <div key={product.id} className='product'> 
-                     <h3>{product.name}</h3>
-                     <img src={product.image} alt={product.name}/>
-                     <div className="details">
-                      <span>{product.desc}</span>
-                      <span className='price'>{product.price}</span>
-                     </div>
-                     <button onClick={()=> handleAddToCart(product)}>Add To Cart</button>
-                  </div>)}
-             </div>
-          </>
-        )}
-        </div>
+    <div className='home-container'>
+      {isLoading ? (
+        <p>Loading ....</p>
+      ) : error ? (
+        <p>An Error Occurred...</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className='products'>
+            {data?.map(product => (
+              <div key={product.id} className='product'>
+                <h3>{product.name}</h3>
+                <img src={product.image} alt={product.name} />
+                <div className="details">
+                  <span>{product.desc}</span>
+                  <span className='price'>{product.price}</span>
+                </div>
+                <button onClick={() => handleAddToCart(product)}>Add To Cart</button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
   );
-}
+};
 
 export default Home;
