5be3928dd45b514f46deedc63b45462ea59ab664
douglanyabasa <douglasnyabasa400@gmail.com>
2025-09-07 17:32:04 +0200

    notification issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <ToastContainer />
         <Navbar />
+        <ToastContainer />
+
         <div className="content-container">
           <Routes>
             <Route exact path="/" element={<Home />} />
